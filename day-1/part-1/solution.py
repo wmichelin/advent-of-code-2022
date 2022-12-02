cc143d07704d5601700e8eea710a0019be97eb76
Walter Michelin <wmichelin@gmail.com>
2022-12-01 20:00:41 -0500

    Add day-1 solution

---
+++ day-1/part-1/solution.py
@@ -0,0 +1,11 @@
+with open("../input.txt", "r") as f:
+    total_calories_by_elf = []
+    curr_elf_calories = []
+    for line in f.readlines():
+        if line == "\n":
+            total_calories_by_elf.append(curr_elf_calories[:])
+            curr_elf_calories = []
+            continue
+        
+        curr_elf_calories.append(int(line))
+    print(f"solution: {sum(max(total_calories_by_elf, key=lambda curr_elf: sum(curr_elf)))}")
